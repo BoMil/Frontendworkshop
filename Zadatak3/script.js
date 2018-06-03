bcc020e104c0b871ffdf9a551fcfc2b62a0f2db1
BoMil <bojanmilicevic91@gmail.com>
2018-06-03 12:31:55 +0200

    Zadatak 3

---
+++ Zadatak3/script.js
@@ -0,0 +1,10 @@
+
+
+ function zbir() {
+    
+let broj1 =document.getElementById('broj1').value;
+let broj2 = document.getElementById('broj2').value;
+
+ document.getElementById('rezultat').value = Number(broj1) + Number(broj2);
+    
+}
